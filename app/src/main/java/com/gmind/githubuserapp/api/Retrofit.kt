428f9473481b3a45d3dc45ae3b82959dde987747
Ilham Gusti Randa <gustiranda1933@gmail.com>
2021-02-08 19:35:10 +0700

    gt

--- app/src/main/java/com/gmind/githubuserapp/api/Retrofit.kt
+++ app/src/main/java/com/gmind/githubuserapp/api/Retrofit.kt
@@ -1,4 +1,15 @@
 package com.gmind.githubuserapp.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object Retrofit {
+    private const val BASE_URL = "https://api.github.com/"
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiInstance = retrofit.create(Api::class.java)
 }
