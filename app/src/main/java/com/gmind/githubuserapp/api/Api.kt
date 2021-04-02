0cd8a5f2bb630366fd9720b31b0c3f31e12353d0
Ilham Gusti Randa <gustiranda1933@gmail.com>
2021-04-02 21:09:55 +0700

    added notiication

--- app/src/main/java/com/gmind/githubuserapp/api/Api.kt
+++ app/src/main/java/com/gmind/githubuserapp/api/Api.kt
@@ -12,35 +12,35 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getSearchUser(
         @Query("q") query: String
     ): Call<SearchResponse>
 
     @GET("users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getListUser(): Call<ArrayList<User>>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getDetailUser(
         @Path("username") username : String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getFollowers(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getFollowing(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/repos")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getRepository(
         @Path("username") username: String
     ): Call<ArrayList<Repository>>
